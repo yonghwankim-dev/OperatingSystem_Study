2ad4efcda5c8d48b4fbf15a7e6fef428d8902b0c
yonghwankim <yonghwankim.dev@gmail.com>
2022-02-28 17:37:45 +0900

    fix the comment pthread_scheduling.c

--- c/chap05_cpu-scheduling/chap05_00_examples/pthread_scheduling.c
+++ c/chap05_cpu-scheduling/chap05_00_examples/pthread_scheduling.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #define NUM_THREADS 5
 
+int sum = 0;
+
 void* runner(void* param);
 
 int main(int argc, char* argv[])
@@ -10,10 +12,10 @@ int main(int argc, char* argv[])
     pthread_t tid[NUM_THREADS];
     pthread_attr_t attr;
 
-    // get the default attributes
+    // 기본 속성 초기화
     pthread_attr_init(&attr);
 
-    // first inquire on the current scope
+    // 경재 범위 정책 확인
     if(pthread_attr_getscope(&attr, &scope) != 0)
     {
         fprintf(stderr, "Unable to get scheduling scope\n");
@@ -22,38 +24,38 @@ int main(int argc, char* argv[])
     {
         if(scope==PTHREAD_SCOPE_PROCESS)
         {
-            printf("PTHREAD SCOPE PROCESS");
+            printf("PTHREAD SCOPE PROCESS\n");
         }
         else if(scope==PTHREAD_SCOPE_SYSTEM)
         {
-            printf("PTHREAD_SCOPE_SYSTEM");
+            printf("PTHREAD_SCOPE_SYSTEM\n");
         }
         else
         {
             fprintf(stderr, "Illegal scope value.\n");
         }
     }
-    // set the scheduling algorithm to PCS or SCS
+    // PCS(프로세스 경쟁 범위) or SCS(시스템 경쟁 범위) 스케줄링 알고리즘 설정
     pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
 
-    // create the threads
+    // 쓰레드 생성
     for(i=0; i<NUM_THREADS; i++)
     {
         pthread_create(&tid[i], &attr, runner, NULL);
     }
 
-    // now join on each thread
+    // 각각의 쓰레드 대기
     for(i=0; i<NUM_THREADS; i++)
     {
         pthread_join(tid[i],NULL);
     }
-
+    printf("sum = %d\n",sum);
 }
 
 // each thread will begin control in the function
 void* runner(void* param)
 {
-    printf("call runner\n");
+    sum += 5;
 
     pthread_exit(0);
 }
